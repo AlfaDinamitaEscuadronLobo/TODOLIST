56b9f74cf0aa8da3b67608ee236bfd3748d3c064
AlfaDinamitaEscuadronLobo <201801300f@utea.edu.pe>
2021-11-19 01:01:03 -0500

    primer commit ing empezando a las 1.01 AM - gaa

---
+++ tareas.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import messagebox
+#from tkinter import font
+
+
+import pymysql
+
+interfaz1= Tk()
+
+interfaz1.geometry("600x900")
+interfaz1.resizable(0,0)
+
+Frame1=Frame(interfaz1,width=1500,height=900)
+Frame1.config(bg="#e0d8c3")
+
+
+
+
+Frame1.pack()
+interfaz1.mainloop()
