f7d4f01c985f70f256a9b55addde5afd71894dff
AlfaDinamitaEscuadronLobo <201801300f@utea.edu.pe>
2021-11-19 03:09:40 -0500

    volviendo a subir por error - SEGUNDA COMMIT 2.1

--- tareas.py
+++ tareas.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import font
 #from tkinter import font
 
 
 import pymysql
 
 interfaz1= Tk()
-
+#-- ventana
 interfaz1.geometry("600x900")
 interfaz1.resizable(0,0)
 
@@ -14,7 +15,19 @@ Frame1=Frame(interfaz1,width=1500,height=900)
 Frame1.config(bg="#e0d8c3")
 
 
+#------LABEL 
 
+img1=PhotoImage(file="iconota.png")
 
+label0img=Label(Frame1,image=img1,bg="#e0d8c3")
+label2=Label(Frame1,text="TO DO",bg="#e0d8c3",font=("Monofonto",40),fg="black")
+
+label3=Label(Frame1,text="Una  aplicacion sencilla para la \ngestion de tareas,Sientase libre \nde expresar sus ideas ",bg="#e0d8c3",font=("Source Code Pro",18),fg="black")
+
+#-----POSIUC
+
+label0img.place(x=70,y=90)
+label2.place(x=230,y=550)
+label3.place(x=50,y=620)
 Frame1.pack()
 interfaz1.mainloop()
