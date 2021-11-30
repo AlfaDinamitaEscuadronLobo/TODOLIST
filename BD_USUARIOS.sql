4f68c21cfbfa5c4dca38ce5ddd1f7264ab625b3a
AlfaDinamitaEscuadronLobo <201801300f@utea.edu.pe>
2021-11-29 23:48:29 -0500

    me olvide de añadir la base de datos ajaj :( :(

---
+++ BD_USUARIOS.sql
@@ -0,0 +1,12 @@
+create database  USARIOSLIST
+use USARIOSLIST;
+create table registrar
+(
+Nombres char(30), 
+Apellidos char(30),
+Correo char(30),
+Nuevo_Usuario char(30),
+Contraseña char(30)
+)
+select* from registrar
+
